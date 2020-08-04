6d085f4b85dee28d6a3db2a8ef1d2a1b690cd0bb
Aditya Goyal <43295337+adityagoyal222@users.noreply.github.com>
2020-08-04 22:12:05 +0545

    added a couple more views

--- blog_site/blog_app/views.py
+++ blog_site/blog_app/views.py
@@ -1,15 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+# To redirect the user to a URL only after delete is completed
+from django.urls import reverse_lazy
+# To ensure user is logged in before accessing content on website (for function-based views)
+from django.contrib.auth.decorators import login_required
+# To ensure user is logged in before accessing content on website (for class-based views)
+from django.contrib.auth.mixins import LoginRequiredMixin 
+#To get pre-existing views from Django
 from django.views.generic import (TemplateView, ListView,
                                  DetailView, CreateView,
                                  UpdateView, DeleteView)
-from blog_app.models import Post, Comment
-from django.utils import timezone
-# To ensure login before accessing content on website
-from django.contrib.auth.mixins import LoginRequiredMixin 
-from blog.forms import PostForm, CommentForm
-from django.urls import reverse_lazy
-# Create your views here.
 
+# Internal imports
+from blog_app.models import Post, Comment
+from blog.forms import PostForm, CommentForm
+
+# Views for Posts and Drafts
 class AboutView(TemplateView):
     template_name = 'about.html'
 
@@ -54,3 +60,38 @@ class DraftListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return Post.objects.filter(published_date__isnull=True).order_by('created_date')
+
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish
+    return redirect('post_detail', pk=pk)
+
+# Views for Comments
+@login_required
+def add_comments_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog_app/comment_form.html', context={'form': form})
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail', pk=post_pk)
